187eafde70ad4b336ef827c9fb25590e8a64a353
peetmaker <peetmaker3627@gmail.com>
2019-04-17 14:00:19 +0700

    second

---
+++ PO_INVOICE/fabcar-channel-base-event/utils/utils/hash.js
@@ -0,0 +1,19 @@
+var crypto = require('crypto');
+
+/**
+ * Create HASH
+ * @method post
+ * @description - Create HASH function
+ * @param {Object}req -data to hash input
+ * @param {Object}res -hash key result output
+ */
+
+exports.hash = function(data){
+
+    var hash = crypto.createHash('sha256')          //HASH FUCTION
+    .update(data)
+    .digest('hex');
+    console.log('HASH Ciphertext Complete!!! : ', data)
+    return data;
+
+}
